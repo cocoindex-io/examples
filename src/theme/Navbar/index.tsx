d74712faa7043b009020400e1635796974bcc325
Linghua Jin <linghua@cocoindex.io>
2026-01-11 19:55:16 -0800

    clean up

--- src/theme/Navbar/index.tsx
+++ src/theme/Navbar/index.tsx
@@ -1,12 +1,58 @@
 import React from 'react';
 import NavbarLayout from '@theme/Navbar/Layout';
-import NavbarContent from './Content';
+import {useThemeConfig, useColorMode} from '@docusaurus/theme-common';
+import {useNavbarMobileSidebar} from '@docusaurus/theme-common/internal';
+import NavbarItem from '@theme/NavbarItem';
+import NavbarColorModeToggle from '@theme/Navbar/ColorModeToggle';
+import SearchBar from '@theme/SearchBar';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import Link from '@docusaurus/Link';
+import {FaBars} from 'react-icons/fa';
+import type {Props as NavbarItemConfig} from '@theme/NavbarItem';
+import styles from './styles.module.css';
 
 export default function Navbar(): React.ReactElement {
+  const mobileSidebar = useNavbarMobileSidebar();
+  const {colorMode} = useColorMode();
+  const {navbar: {items, logo, title}} = useThemeConfig();
+  const logoSrc = colorMode === 'dark' ? 'img/logo-dark.svg' : 'img/logo.svg';
+  const leftItems = items.filter(item => item.position === 'left');
+  const rightItems = items.filter(item => item.position === 'right');
+
   return (
     <NavbarLayout>
-      <NavbarContent />
+      <div className={styles.navbarContainer}>
+        <button
+          className={styles.navbarMobileMenuButton}
+          onClick={mobileSidebar.toggle}
+          aria-label="Toggle navigation bar"
+          aria-expanded={mobileSidebar.shown}
+        >
+          <FaBars className={styles.navbarMobileMenuIcon} />
+        </button>
+
+        <div className={styles.navbarBrand}>
+          <Link to={logo?.href || '/'} className={styles.navbarLogoLink}>
+            <img
+              className={styles.navbarLogo}
+              src={useBaseUrl(logoSrc)}
+              alt={logo?.alt || title}
+            />
+          </Link>
+        </div>
+
+        <div className={styles.navbarItemsLeft}>
+          {leftItems.map((item, i) => <NavbarItem {...(item as NavbarItemConfig)} key={i} />)}
+        </div>
+
+        <div className={styles.navbarItemsRight}>
+          <div className={styles.navbarSearch}>
+            <SearchBar />
+          </div>
+          {rightItems.map((item, i) => <NavbarItem {...(item as NavbarItemConfig)} key={i} />)}
+          <NavbarColorModeToggle className={styles.colorModeToggle} />
+        </div>
+      </div>
     </NavbarLayout>
   );
 }
-
